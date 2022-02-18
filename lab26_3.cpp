0a2d27840025181bc82e834b3d0497157253ebdb
BoegyNat <bboegy@gmail.com>
2022-02-18 17:44:41 +0700

    Finished Lab26

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
+int count(int a[],int N){
+	set<int> b;
+	for(int i=0;i<N;i++){	
+		b.insert(a[i]);
+	}
+	return b.size();
+}
 //Write function count() here.
 
 int main(){	
